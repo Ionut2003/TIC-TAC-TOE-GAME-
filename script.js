3a5bbd1f4f68531066fb851f0977524af96f1aa7
Ionut Paraschiv <ionutparaschiv360@gmail.com>
2024-05-01 15:11:29 +0300

    little css and javascript changes and made it responsive

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const huPlayer = 'X';
 let availSpots = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 const restartButton = document.getElementById("restart");
-startGame();
-allCells.forEach(cell => cell.addEventListener('mouseup', playerTurn));
-restartButton.addEventListener('mouseup', restartAllCells);
+startGame(); //starting the game
+allCells.forEach(cell => cell.addEventListener('click', playerTurn)); // click event for every table cell
+document.getElementById("table").style.height = document.getElementById("table").clientWidth + "px"; // making the table square
+restartButton.addEventListener('click', startGame); // restarting the board
 function checkTie(){
 	a = board.filter(e => typeof(e) === 'number');
 	if(a.length === 0 && !checkWin(huPlayer, board)){
@@ -15,7 +16,7 @@ function checkTie(){
 	}
 	return false;
 }
-function playerTurn(event){
+function playerTurn(){
 	makeMove(huPlayer, board);
 }
 function gameWin(player, brd){
@@ -27,22 +28,23 @@ function gameWin(player, brd){
 			if(brd[i] === player) plays.push(i);
 		}
 		for(let j of wins){
-		for(let i of j){
-			if(plays.indexOf(i) !== -1) win.push(i);
+			for(let i of j){
+				if(plays.indexOf(i) !== -1) win.push(i);
+			}
+			if(win.length === 3){
+				return true;
+			}
+			win = [];
 		}
-		if(win.length === 3){
-			return true;
-		}
-		win = [];
-		}
-return false;
+	return false;
 }
 function checkWin(player, brd){
 		let win = [];
-		let wins = [[0, 1, 2],	[3, 4, 5],	[6, 7, 8],		[0, 3, 6],		[1, 4, 7],		[2, 5, 8],		[0, 4, 8],		[2, 4, 6]];
+		let wins = [
+			[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8],	[2, 4, 6]
+		]; // all the variants of wins
 		let plays = [];
-		let l = brd.length;
-		for(let i = 0; i<l; i++){
+		for(let i = 0; i<brd.length; i++){
 			if(brd[i] === player) plays.push(i);
 		}
 		for(let j of wins){
@@ -57,7 +59,6 @@ function checkWin(player, brd){
 		}
 		win = [];
 		}
-		console.log(brd === board);
 return false;
 }
 function declareWinner(who){
@@ -65,52 +66,42 @@ function declareWinner(who){
 	(who === huPlayer) ? alert('Game win!!!!') : alert('Game lose!!!');
 	}
 }
-function restartAllCells(event){
-	for(i = 0; i<9; i++){
-		allCells[i].innerHTML = '';
-		allCells[i].style.backgroundColor = 'yellow';
-		availSpots.push(i);
-		board[i] = i;	
-	}
-}
+
 function startGame(){
 	for(let i = 0; i<9; i++){
 		allCells[i].innerHTML = '';
-		allCells[i].style.backgroundColor = 'yellow';
-		availSpots.push(i);
+		availSpots[i] = i;
 		board[i] = i;
+		allCells[i].style.backgroundColor = "yellow";
 	}
 }
 function makeMove(player, newbrd){
 	if(player === huPlayer){
 	var currentCell = parseInt(event.target.id);
 	var addToken = document.getElementById(`${currentCell}`);
-		if(addToken.innerHTML === 'X' || addToken.innerHTML === 'O'){
-			makeMove(huPlayer, board);
-		}
-		else{
+		if(addToken.innerHTML !== 'X' && addToken.innerHTML !== 'O') {
 			if((!checkWin(huPlayer, board) && !checkWin(aiPlayer, board)) && !checkTie()){
-			addToken.style.color = 'red';
-			addToken.innerHTML = player;
-			board[currentCell] = player;
-			availSpots = availSpots.filter(value => value !== currentCell);
-			if(!checkWin(huPlayer, board) && !checkTie()) makeMove(aiPlayer, board);
+				addToken.style.color = 'red';
+				addToken.innerHTML = player;
+				board[currentCell] = player;
+				availSpots = availSpots.filter(value => value !== currentCell);
+				if(!checkWin(huPlayer, board) && !checkTie()) makeMove(aiPlayer, board);
 			}
 			else 
 			{
 				allCells.forEach(cell => cell.removerEventListener('click', playerTurn));
 			}
 		}
-}
-else{
-	let theCell = minimax(board, aiPlayer).index;
-	let addAi = document.getElementById(`${theCell}`);
-	addAi.style.color = "lime";
-	addAi.innerHTML = player;
-	board[theCell] = player;
-	availSpots = availSpots.filter(value => value !== theCell);
-}
-declareWinner(player);
+	}
+	else{
+		let theCell = minimax(board, aiPlayer).index;
+		let addAi = document.getElementById(`${theCell}`);
+		addAi.style.color = "lime";
+		addAi.innerHTML = player;
+		board[theCell] = player;
+		availSpots = availSpots.filter(value => value !== theCell);
+	}
+	declareWinner(player);
 }
 
 function minimax(nBoard, ply){
